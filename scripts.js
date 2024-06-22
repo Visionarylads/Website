534bf8d85855aaaee649fa281690d517093c0699
Visionarylads <ourklassrooom@gmail.com>
2024-06-22 21:28:45 +0530

    Add files via upload
    This is a start

---
+++ scripts.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const headerHeight = document.querySelector('.container').offsetHeight;
+
+    document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - headerHeight,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
